b93dcd0b5b69e2b2d9571385160399f6ae1a743e
fosterhordyk290 <fosterhordyk290@gmail.com>
2025-04-01 16:06:35 +0000

    Add 24.php

---
+++ src/24.php
@@ -0,0 +1,11 @@
+<?php
+// Define a class with methods
+class MathOperations {
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+}
