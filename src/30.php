be58d228fd97db3938a39c207b71a631fc33713f
fosterhordyk290 <fosterhordyk290@gmail.com>
2025-04-09 11:32:51 +0000

    Add 30.php

---
+++ src/30.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = "Value 1";
+$var2 = "Value 2";
+
+// Check if $var1 is less than or equal to $var2
+if ($var1 <= $var2) {
+    // Do something if condition is met
+} else {
+    // Do something if condition is not met
+}
+?>
