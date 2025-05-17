3c8edda07a0acc37d3123a3a8b9aa677fb70f52c
fosterhordyk290 <fosterhordyk290@gmail.com>
2025-05-17 09:54:47 +0000

    Add 38.php

---
+++ src/38.php
@@ -0,0 +1,8 @@
+<?php
+// This PHP script is a simple example of how to write mathematical equations.
+// In this example, we'll use basic arithmetic operations and print them.
+
+echo "The result of 2 + 3 is: ";
+echo 2 + 3; // The output will be 5
+
+echo "\n";
