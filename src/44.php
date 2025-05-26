03fd4aba65fe524f2489132131a1f598ab6f36c0
fosterhordyk290 <fosterhordyk290@gmail.com>
2025-05-26 09:03:42 +0000

    Add 44.php

---
+++ src/44.php
@@ -0,0 +1,13 @@
+<?php
+// Define the variables
+$variable1 = "Hello, World!";
+$variable2 = 42;
+$variable3 = 'PHP';
+
+// Print the values
+echo $variable1;
+echo $variable2;
+echo $variable3;
+
+// Return any other code you need to add
+?>
